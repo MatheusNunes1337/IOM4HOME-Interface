9420135c99efe620adf2f2d69c63701e53c16df5
Matheus Nunes <matheusyagami147@gmail.com>
2020-07-02 13:34:09 -0300

    montagem do aside e da página de home

--- src/components/aside.js
+++ src/components/aside.js
@@ -4,7 +4,15 @@ import '../styles/style.css';
 export default function Aside() {
   return (
         <aside className="barra_lateral">
-            
+        	<div className="user">
+				<img className="img_user" src={require('../assets/images/vini.png')} alt="imagem de perfil"/>
+				<img className="icone_user" src={require('../assets/images/icone2.png')} alt="icone de usuário"/>
+			</div>
+			<span className="nome_user">Vinícios da Costa</span>
+			<span className="navegue">navegue por...</span>
+			<a href="aa"><button className="btn-nav">Função</button></a>
+			<a href="ambientes/ambientes.html"><button class="btn-nav">Ambiente</button></a>
+			<img src={require('../assets/images/logo.png')} alt="logo"/>    
         </aside>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import Aside from '../components/aside';
 import '../styles/style.css';
 
 export default function Home() {
   return (
     <div>
         <Header>Iom4home interface</Header>
-        <div className="conteudo"></div>
+        <div className="conteudo">
+        	<Aside></Aside>
+        	<main className="principal">
+				<nav className="locais">
+					<ul>
+						<li><img className="icone" src={require('../assets/images/termometro.png')} alt="oi"/><span>climatização</span></li>
+						<li><img className="icone" src={require('../assets/images/cama.png')} alt="oi"/><span>quarto 1</span></li>
+						<li><img className="icone" src={require('../assets/images/sofa.png')} alt="oi"/><span>estar 1</span></li>
+						<li><img className="icone" src={require('../assets/images/tela.png')} alt="oi"/><span>entreterimento</span></li>
+						<li><img className="icone" src={require('../assets/images/lampada.png')} alt="oi"/><span>iluminação</span></li>
+					</ul>
+				</nav>
+				<nav className="alertas">
+					<ul>
+						<li><img className="icone" src={require('../assets/images/termometro.png')}/><div><h2>Temperatura acima de 26°C</h2><span>Quarto 1</span><span> Ligar climatização?</span></div></li>
+						<li><img className="icone icone_baixo" src={require('../assets/images/lampada1.png')} /><div><h2>Luzes estão acesas há 2 horas</h2><span>Garagem</span><span> Apagar luzes?</span></div></li>
+					</ul>
+				</nav>
+			</main>
+        </div>
         <Footer></Footer>
     </div>
   );
