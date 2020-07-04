d8e39e845059507d429b3c9b4809b1316e6f89ce
Matheus Nunes <matheusyagami147@gmail.com>
2020-07-04 00:07:01 -0300

    rotas implementadas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
 
 //import Routes from './routes';
-import Ambiente from './pages/ambiente'
+import Ambiente from './pages/ambiente';
+import Home from './pages/home';
 
 
 function App() {
   return (
-  	<Ambiente />
+  	 <Router> 
+  	 	<Switch>
+  	 		<Route path="/" exact component={Ambiente} />
+  	 		<Route path="/oi" component={Home} />
+  	 	</Switch>	
+  	 </Router>
   );
 }
 
