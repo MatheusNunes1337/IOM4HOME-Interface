5ccded346bcf37419ef13286e95362e454d4741d
Matheus Nunes <matheusyagami147@gmail.com>
2020-07-03 23:43:10 -0300

    alterações na div principal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Home from './pages/home';
-import Ambiente from './pages/ambiente';
 
+//import Routes from './routes';
+import Ambiente from './pages/ambiente'
 
 
 function App() {
   return (
-    <Ambiente></Ambiente>
+  	<Ambiente />
   );
 }
 
