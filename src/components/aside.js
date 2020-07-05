f5405a33bd7c544ced84e24553ccb592bf87091e
Matheus Nunes <matheusyagami147@gmail.com>
2020-07-04 23:21:35 -0300

    alteração da tag a para link no menu da barra lateral

--- src/components/aside.js
+++ src/components/aside.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import '../styles/style.css';
 
+import { Link } from 'react-router-dom';
+
+import { FaChevronDown, FaCircle } from "react-icons/fa";
+
 export default function Aside() {
   return (
         <aside className="barra_lateral">
         	<div className="user">
 				<img className="img_user" src={require('../assets/images/vini.png')} alt="imagem de perfil"/>
-				<img className="icone_user" src={require('../assets/images/icone2.png')} alt="icone de usuário"/>
+				<button className="icone_user"><FaChevronDown color="#c2c3bf" size="20px" /></button>
 			</div>
 			<span className="nome_user">Vinícios da Costa</span>
 			<span className="navegue">navegue por...</span>
-			<a href="aa"><button className="btn-nav">Função</button></a>
-			<a href="ambientes/ambientes.html"><button className="btn-nav">Ambiente</button></a>
+			<Link to="/aa" style={{ textDecoration: 'none'}}><button className="btn-nav">Função</button></Link>
+			<Link to="/ambientes" style={{ textDecoration: 'none' }}><button className="btn-nav">Ambiente</button></Link>
+			<FaCircle color="#1f703d" size="15px" /><span className="conection_status">Connected</span>
 			<img src={require('../assets/images/logo.png')} alt="logo"/>    
         </aside>
   );
