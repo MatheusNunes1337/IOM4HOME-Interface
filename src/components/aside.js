acbea259a9d7ba503f51f19278a002c76d3df483
Matheus Nunes <matheusyagami147@gmail.com>
2020-07-13 15:15:55 -0300

    adicionada mudança de estado nos botões da barra lateral

--- src/components/aside.js
+++ src/components/aside.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/style.css';
 
 import { Link } from 'react-router-dom';
@@ -6,6 +6,35 @@ import { Link } from 'react-router-dom';
 import { FaChevronDown, FaCircle } from "react-icons/fa";
 
 export default function Aside() {
+
+	let button;
+
+
+	let [btn_funcao, setButton1] = useState('desativado');
+	let [btn_ambiente, setButton2] = useState('desativado');
+
+	function btnPushed(e) {
+		button = e.currentTarget.value;
+		console.log(button);
+
+		if(button === 'ambiente') {
+			if(btn_ambiente === 'desativado'){
+				setButton2(btn_ambiente = 'ativado')
+				if(btn_funcao === 'ativado') {
+					setButton1(btn_funcao = 'desativado')
+				} 
+			} 
+		} else {
+			if(btn_funcao === 'desativado') {
+				setButton1(btn_funcao = 'ativado')
+				if(btn_ambiente === 'ativado') {
+					setButton2(btn_ambiente = 'desativado')
+				}	
+			}  
+		}
+	}
+
+
   return (
         <aside className="barra_lateral">
         	<div className="user">
@@ -14,8 +43,10 @@ export default function Aside() {
 			</div>
 			<span className="nome_user">Vinícios da Costa</span>
 			<span className="navegue">navegue por...</span>
-			<Link to="/aa" style={{ textDecoration: 'none'}}><button className="btn-nav">Função</button></Link>
-			<Link to="/ambientes" style={{ textDecoration: 'none' }}><button className="btn-nav">Ambiente</button></Link>
+			<Link to="/aa" style={{ textDecoration: 'none'}}><button value="funcao"
+			className={"btn-nav" + ' ' + btn_funcao} onClick={(e) => btnPushed(e)}>Função</button></Link>
+			<Link to="/ambientes" style={{ textDecoration: 'none' }}><button value="ambiente"
+			className={"btn-nav" + ' ' + btn_ambiente} onClick={(e) => btnPushed(e)}>Ambiente</button></Link>
 			<FaCircle color="#1f703d" size="15px" /><span className="conection_status">Connected</span>
 			<img src={require('../assets/images/logo.png')} alt="logo" id="iom_logo" />    
         </aside>
