d033250b4aa63f1a4be470f5aaf7a1673cc68b47
Matheus Nunes <matheusyagami147@gmail.com>
2020-08-06 15:26:46 -0300

    componente de equipamentos completo

---
+++ src/components/equipamentos.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import '../styles/style.css';
+import ActionBtn from './actionButton'
+import { Link } from 'react-router-dom'
+
+export default function Equipamentos(props) {
+
+	let equipamentos = [];
+	let icone
+	let formatted
+
+	props.equipamentos.map(pushEquip)
+
+	function pushEquip(equip, i) {
+
+		let formatted = equip.split(" ");
+		if(formatted.length === 2) {
+			formatted =  formatted.join('_')	
+		}
+
+		
+		const iniciais = equip.slice(0,3)
+
+		switch (iniciais) {
+			case 'all':
+				icone = 'lamp'
+				break
+			case 'lam':
+				icone = 'lamp'
+				break
+			case 'tv':
+				icone = 'tv'
+				break
+			case 'air':
+				icone = 'ar'
+				break
+			case 'sou':
+				icone = 'sound'
+				break					
+		}
+
+		equipamentos.push(<ActionBtn description={equip} key={i} equipamento={formatted} icon={icone}/>)
+	}
+
+
+	return (
+		<div className="funcoes">
+			{equipamentos}
+		</div>
+	);
+}
--- src/pages/bedroom1.js
+++ src/pages/bedroom1.js
@@ -4,6 +4,7 @@ import Footer from '../components/footer';
 import Aside from '../components/aside';
 import ActionBtn from '../components/actionButton';
 import Comodos from '../components/comodos'
+import Equipamentos from '../components/equipamentos'
 
 import { Link } from 'react-router-dom'
 import '../styles/style.css';
@@ -17,15 +18,7 @@ export default function Bedroom1() {
         	<Aside></Aside>
         	<main className="principal">
         		<Comodos itens={['bedroom 2', 'living room', 'kitchen', 'office', 'bedroom 3']} />
-				<div className="funcoes">
-					<ActionBtn description="air conditioning" equipamento="ar_condicionado" icon="ar"/>
-					<ActionBtn description="lamp 1" equipamento="lamp_1" icon="lamp"/>
-					<ActionBtn description="lamp 2" equipamento="lamp_2" icon="lamp"/>
-					<ActionBtn description="lampshade" equipamento="lampshade" icon="lamp"/>
-					<ActionBtn description="all lamps" equipamento="all_lamps" icon="lamp" />
-					<ActionBtn description="tv" equipamento="tv" icon="tv"/>
-					<ActionBtn description="sound" equipamento="sound" icon="sound"/>		
-				</div>
+        		<Equipamentos equipamentos={['air conditioning', 'lamp 1', 'lamp 2', 'lampshade', 'all lamps', 'tv', 'sound']} />
 			</main>
         </div>
         <Footer></Footer>
